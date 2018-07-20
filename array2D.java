3233769c9a070acf759cf7a3a523c672b579f9fb
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-20 12:24:42 +0530

    Create array2D.java

---
+++ array2D.java
@@ -0,0 +1,17 @@
+//Not yet completed
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int arr[][] = new int[100][100],i,j, sum =0;
+	    Scanner in = new Scanner(Systerm.in);
+	    for(i = 0;i<6;i++)
+	    {
+	        for(j=0;j<6;j++)
+	        {
+	            arr[i][j] = in.nextInt();
+	        }
+	    }
+	}
+}
