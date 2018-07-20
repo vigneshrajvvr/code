c3a5d8053c3ae14fee7ce837c9154072e217fdf0
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-20 12:36:58 +0530

    Update array2D.java

--- array2D.java
+++ array2D.java
@@ -4,8 +4,8 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-	    int arr[][] = new int[100][100],i,j, sum =0;
-	    Scanner in = new Scanner(Systerm.in);
+	    int arr[][] = new int[100][100],i,j, sum =0,O1=0,I1=0,O2=3,I2=3,k;
+	    Scanner in = new Scanner(System.in);
 	    for(i = 0;i<6;i++)
 	    {
 	        for(j=0;j<6;j++)
@@ -13,5 +13,21 @@ public class Main
 	            arr[i][j] = in.nextInt();
 	        }
 	    }
+	    for(k=0;k<4;k++)
+	    {
+	        for(i =O1;i<O2;i++)
+	        {
+	            for(j=I1;j<I2;j++)
+	            {
+	                sum =sum + arr[i][j];
+	            }
+	        }
+	        O1++;
+	        O2++;
+	        I1++;
+	        I2++;
+	        System.out.println(""+sum);
+	    }
 	}
 }
+
